bd7087c2e5c10750bf88f146f6db81c7a3b0bfa7
Kamol Treewatchararat <tholapz@gmail.com>
2013-11-24 02:10:42 -0800

    before testing

--- square2.js
+++ square2.js
@@ -13,37 +13,57 @@ fs.readFile(process.argv[2], 'utf8', function  (err, data) {
 		var first = [-1, -1]
 		var last = [-1, -1]
 		var N = parseInt(lines.shift())
-		for(var j = 0; j < N; j++) {
+		var failed = false
+		(for(var j = 0; j < N; j++) {
 			var row = lines.shift().split('')
 			var isBlack = false
 			for(var k = 0; k < N; k++) {
+				if(size > 0) {
+					if(!isCorrect(j,k, size, first[0], first[1], row[k]))
+						failed = true
+						return
+				}
 				if(row[k] === '#') {
 					totalHashes++
 					isBlack = true
 					if(first === [-1, -1]) {
 						first = [j,k]
 					}
-
 				} else {
 					if(isBlack){
 						isBlack = false
-						if(last === [-1,-1]) {
-							last = [j,k]
-							size = first[1] - last[1] + 1
+						if(size === 0) {
+							size = first[1] - k + 1
 						}
 					}
 				}
 
 			}
-		}
+		})()
 
-		if(!(Math.sqrt(totalHashes) % 1 === 0)) {
+		if(size<=0) {
+			//no hash was found
 			out = out.concat('NO')
-			break
+		} else if(!(Math.sqrt(totalHashes) % 1 === 0)) { 
+			//hash count check
+			out = out.concat('NO')
+		} else if( failed ) {
+			out = out.concat('NO')
+		}else {
+			out = out.concat('YES')
 		}
-
-
 
 		console.log(out)
 	}
 })
+
+function isCorrect (row, col, size, row1, col1, val) {
+	// check row
+	if(row >= row1 && row <= (row1 + size - 1)) {
+		//check col
+		if(col >= col1 && col <= (col1 + size - 1)) {
+			return val === '#'
+		}
+	}
+	return val === '.'
+}
